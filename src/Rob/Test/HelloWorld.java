f949b78013cd88fcd85c67e7352cb4675c758000
rpassmore <rpassmore@PC1>
2011-02-25 15:36:08 +0000

    Initial commit

---
+++ src/Rob/Test/HelloWorld.java
@@ -0,0 +1,18 @@
+package Rob.Test;
+
+import android.app.Activity;
+import android.widget.TextView;
+import android.os.Bundle;
+
+public class HelloWorld extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        TextView tv = new TextView(this);
+        tv.setText("Hello, Android");
+        setContentView(tv);
+    }
+}
